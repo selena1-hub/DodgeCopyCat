4cebf7179af7b42c6c2e646c97643f147e76bc79
selena1-hub <pionna761@gmail.com>
2025-09-10 10:23:24 +0900

    2025/09/10/10:22
    깃허브 연동 테스트

---
+++ Assets/02.Scripts/GithhubTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GithhubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //2(o/\/o}!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
